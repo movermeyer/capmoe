d1119955e7baa796d19941f6eaaddbca8fed622b
Sho Nakatani <lay.sakura@gmail.com>
2014-02-09 15:46:01 +0900

    wip

--- sandbox/circles_detection.py
+++ sandbox/circles_detection.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 ''' file name : houghcircles.py
 
 Description : This sample shows how to detect circles in image with Hough Transform
@@ -15,27 +16,61 @@ Written by : Abid K. (abidrahman2@gmail.com) , Visit opencvpython.blogspot.com f
 import cv2
 import numpy as np
 import sys
+from random import randint
 
-if len(sys.argv)>1:
-    filename = sys.argv[1]
-else:
-    filename = 'board.jpg'
 
-img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-if img==None:
-    print "cannot open ",filename
+if __name__ == '__main__':
+    if len(sys.argv)>1:
+        filename = sys.argv[1]
+    else:
+        filename = 'board.jpg'
 
-else:
+    img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    if img==None:
+        print "cannot open ",filename
+        sys.exit(1)
+
+    img_height, img_width = img.shape[0:2]
+
+    # 本流の処理
     # img = cv2.medianBlur(img,5)
-    cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-    circles = cv2.HoughCircles(img,cv2.cv.CV_HOUGH_GRADIENT,1,100,param1=100,param2=30,
-                               minRadius=45,
-                               # maxRadius=20
+    img = cv2.GaussianBlur(img,
+                           (5,5),  # param
+                           0       # param
+    )
+
+    circles = cv2.HoughCircles(  # 返り値には確度の大きいもの順に入ってるらしい
+        img, cv2.cv.CV_HOUGH_GRADIENT,
+        dp=1,
+        minDist=1,
+        # minDist=int(min(img_width, img_height) * 0.2),
+        param1=85, param2=40,
+        minRadius=int(min(img_width, img_height) * 0.2),  # なるべく大きく欲しい王冠写してよ
+        maxRadius=int(min(img_width, img_height) * 0.6),  # 画像はみ出す円はいらない
     )
     circles = np.uint16(np.around(circles))
-    for i in circles[0,:]:
-        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),1) # draw the outer circle
-        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3) # draw the center of the circle
+
+    max_candidates = 5
+    i_candidate = 0
+    cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    for x, y, r in circles[0,:]:
+        # 画像をはみ出す円はいらない
+        # if x - r < 0 or x + r > img_width or y - r < 0 or y + r > img_height:
+        #     continue
+
+        # 写真中心に近い円だけが欲しい
+        img_center_x, img_center_y = (img_width / 2, img_height / 2)
+        threshold_r = min(img_width, img_height) * 0.2
+        if (x - img_center_x) ** 2 + (y - img_center_y) ** 2 > threshold_r ** 2:
+            continue
+
+        rgb = (randint(0, 255), randint(0, 255), randint(0, 255))
+        cv2.circle(cimg,(x, y), r, rgb, 1)   # draw the outer circle
+        cv2.putText(cimg, str(i_candidate), (x, y), cv2.FONT_HERSHEY_PLAIN, fontScale=2.0, color=rgb)
+        # cv2.circle(cimg,(x, y), 2, (0, 255, 0), 3) # draw the center of the circle
+        i_candidate += 1
+        if i_candidate == max_candidates:
+            break
 
     cv2.imshow('detected circles',cimg)
     cv2.waitKey(0)
--- sandbox/surf_test.py
+++ sandbox/surf_test.py
@@ -1,68 +1,145 @@
-# matching features of two images
+#!/usr/bin/env python
+
+'''
+Uses SURF to match two images.
+
+Based on the sample code from opencv:
+  samples/python2/find_obj.py
+
+USAGE
+  find_obj.py <image1> <image2>
+'''
+
+import numpy
 import cv2
+
 import sys
-import scipy as sp
- 
-if len(sys.argv) < 3:
-    print 'usage: %s img1 img2' % sys.argv[0]
-    sys.exit(1)
- 
-img1_path = sys.argv[1]
-img2_path = sys.argv[2]
- 
-img1 = cv2.imread(img1_path, cv2.CV_LOAD_IMAGE_GRAYSCALE)
-img2 = cv2.imread(img2_path, cv2.CV_LOAD_IMAGE_GRAYSCALE)
- 
-detector = cv2.FeatureDetector_create("SURF")
-descriptor = cv2.DescriptorExtractor_create("BRIEF")
-matcher = cv2.DescriptorMatcher_create("BruteForce-Hamming")
- 
-# detect keypoints
-kp1 = detector.detect(img1)
-kp2 = detector.detect(img2)
- 
-print '#keypoints in image1: %d, image2: %d' % (len(kp1), len(kp2))
- 
-# descriptors
-k1, d1 = descriptor.compute(img1, kp1)
-k2, d2 = descriptor.compute(img2, kp2)
- 
-print '#keypoints in image1: %d, image2: %d' % (len(d1), len(d2))
- 
-# match the keypoints
-matches = matcher.match(d1, d2)
- 
-# visualize the matches
-print '#matches:', len(matches)
-dist = [m.distance for m in matches]
- 
-print 'distance: min: %.3f' % min(dist)
-print 'distance: mean: %.3f' % (sum(dist) / len(dist))
-print 'distance: max: %.3f' % max(dist)
- 
-# threshold: half the mean
-thres_dist = (sum(dist) / len(dist)) * 0.5
- 
-# keep only the reasonable matches
-sel_matches = [m for m in matches if m.distance < thres_dist]
- 
-print '#selected matches:', len(sel_matches)
- 
-# #####################################
-# visualization
-h1, w1 = img1.shape[:2]
-h2, w2 = img2.shape[:2]
-view = sp.zeros((max(h1, h2), w1 + w2, 3), sp.uint8)
-view[:h1, :w1, 0] = img1
-view[:h2, w1:, 0] = img2
-view[:, :, 1] = view[:, :, 0]
-view[:, :, 2] = view[:, :, 0]
- 
-for m in sel_matches:
-    # draw the keypoints
-    # print m.queryIdx, m.trainIdx, m.distance
-    color = tuple([sp.random.randint(0, 255) for _ in xrange(3)])
-    cv2.line(view, (int(k1[m.queryIdx].pt[0]),int(k1[m.queryIdx].pt[1])), (int(k2[m.trainIdx].pt[0] + w1), int(k2[m.trainIdx].pt[1])), color)
- 
-cv2.imshow("view", view)
-cv2.waitKey()
+
+###############################################################################
+# Image Matching
+###############################################################################
+
+def match_images(img1, img2):
+    """Given two images, returns the matches"""
+    detector = cv2.SURF(400, 5, 5)
+    matcher = cv2.BFMatcher(cv2.NORM_L2)
+
+    kp1, desc1 = detector.detectAndCompute(img1, None)
+    kp2, desc2 = detector.detectAndCompute(img2, None)
+    #print 'img1 - %d features, img2 - %d features' % (len(kp1), len(kp2))
+
+    raw_matches = matcher.knnMatch(desc1, trainDescriptors = desc2, k = 2) #2
+    kp_pairs = filter_matches(kp1, kp2, raw_matches)
+    return kp_pairs
+
+def filter_matches(kp1, kp2, matches, ratio = 0.4):
+    mkp1, mkp2 = [], []
+    for m in matches:
+        if len(m) == 2 and m[0].distance < m[1].distance * ratio:
+            m = m[0]
+            mkp1.append( kp1[m.queryIdx] )
+            mkp2.append( kp2[m.trainIdx] )
+    kp_pairs = zip(mkp1, mkp2)
+    return kp_pairs
+    
+    
+###############################################################################
+# Match Diplaying
+###############################################################################
+
+def explore_match(win, img1, img2, kp_pairs, status = None, H = None):
+    h1, w1 = img1.shape[:2]
+    h2, w2 = img2.shape[:2]
+    vis = numpy.zeros((max(h1, h2), w1+w2), numpy.uint8)
+    vis[:h1, :w1] = img1
+    vis[:h2, w1:w1+w2] = img2
+    vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
+
+    if H is not None:
+        corners = numpy.float32([[0, 0], [w1, 0], [w1, h1], [0, h1]])
+        corners = numpy.int32( cv2.perspectiveTransform(corners.reshape(1, -1, 2), H).reshape(-1, 2) + (w1, 0) )
+        cv2.polylines(vis, [corners], True, (255, 255, 255))
+
+    if status is None:
+        status = numpy.ones(len(kp_pairs), numpy.bool_)
+    p1 = numpy.int32([kpp[0].pt for kpp in kp_pairs])
+    p2 = numpy.int32([kpp[1].pt for kpp in kp_pairs]) + (w1, 0)
+
+    green = (0, 255, 0)
+    red = (0, 0, 255)
+    white = (255, 255, 255)
+    kp_color = (51, 103, 236)
+    for (x1, y1), (x2, y2), inlier in zip(p1, p2, status):
+        if inlier:
+            col = green
+            cv2.circle(vis, (x1, y1), 2, col, -1)
+            cv2.circle(vis, (x2, y2), 2, col, -1)
+        else:
+            col = red
+            r = 2
+            thickness = 3
+            cv2.line(vis, (x1-r, y1-r), (x1+r, y1+r), col, thickness)
+            cv2.line(vis, (x1-r, y1+r), (x1+r, y1-r), col, thickness)
+            cv2.line(vis, (x2-r, y2-r), (x2+r, y2+r), col, thickness)
+            cv2.line(vis, (x2-r, y2+r), (x2+r, y2-r), col, thickness)
+    vis0 = vis.copy()
+    for (x1, y1), (x2, y2), inlier in zip(p1, p2, status):
+        if inlier:
+            cv2.line(vis, (x1, y1), (x2, y2), green)
+
+    cv2.imshow(win, vis)
+
+
+  
+def draw_matches(window_name, kp_pairs, img1, img2):
+    """Draws the matches for """
+    mkp1, mkp2 = zip(*kp_pairs)
+    
+    p1 = numpy.float32([kp.pt for kp in mkp1])
+    p2 = numpy.float32([kp.pt for kp in mkp2])
+    
+    if len(kp_pairs) >= 4:
+        H, status = cv2.findHomography(p1, p2, cv2.RANSAC, 5.0)
+        #print '%d / %d  inliers/matched' % (numpy.sum(status), len(status))
+    else:
+        H, status = None, None
+        #print '%d matches found, not enough for homography estimation' % len(p1)
+    
+    if len(p1):
+        explore_match(window_name, img1, img2, kp_pairs, status, H)
+
+###############################################################################
+# Test Main
+###############################################################################
+
+if __name__ == '__main__':
+    """Test code: Uses the two specified"""
+    if len(sys.argv) < 3:
+        print "No filenames specified"
+        print "USAGE: find_obj.py <image1> <image2>"
+        sys.exit(1)
+    
+    fn1 = sys.argv[1]
+    fn2 = sys.argv[2]
+
+    img1 = cv2.imread(fn1, 0)
+    img2 = cv2.imread(fn2, 0)
+    
+    if img1 is None:
+        print 'Failed to load fn1:', fn1
+        sys.exit(1)
+        
+    if img2 is None:
+        print 'Failed to load fn2:', fn2
+        sys.exit(1)
+
+    kp_pairs = match_images(img1, img2)
+    
+    if kp_pairs:
+        draw_matches('find_obj', kp_pairs, img1, img2)
+        cv2.waitKey()
+        cv2.destroyAllWindows()    
+else:
+        print "No matches found"
+    
+    

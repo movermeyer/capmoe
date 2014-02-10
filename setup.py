f0c9e224f62ffbaf4afe8876d78bf5326b1af4db
Sho Nakatani <lay.sakura@gmail.com>
2014-02-10 10:19:52 +0900

    modifies release info

--- setup.py
+++ setup.py
@@ -3,34 +3,44 @@
 from setuptools import setup
 
 
+tests_require = [
+    'nose',
+    'coverage',
+    'nose-cov',
+    'nose-parameterized',
+],
+
 setup(
     name             = 'capmoe',
-    description      = 'CapMoe - Cap crown searcher',
+    description      = 'CapMoe - Cap Beer cap image search, CLI tools',
     long_description = open('README.rst').read(),
     url              = 'https://github.com/laysakura/capmoe',
-    license          = 'LICENSE.txt',
+    # license          = 'LICENSE.txt',
     version          = '0.0.1',
     author           = 'Sho Nakatani',
     author_email     = 'lay.sakura@gmail.com',
+    tests_require    = tests_require,
     install_requires = [
         'pillow',
+        'scipy',
         'numpy',
+        'simplejson',
     ],
-    tests_require    = [
-        'nose',
-        'coverage',
-        'nose-cov',
-    ],
-    packages         = [
+    extras_require = {
+        'testing': tests_require,
+    },
+    packages = [
         'capmoe',
+        'capmoe.api',
+        'capmoe.cv',
     ],
-    scripts          = [
+    scripts = [
+        'bin/capmoe-capdetector.py',
     ],
-    classifiers      = '''
+    classifiers = '''
 Programming Language :: Python
 Development Status :: 1 - Planning
-License :: OSI Approved :: BSD License
-Programming Language :: Python :: 3.2
-Programming Language :: Python :: 3.3
+Programming Language :: Python :: 2.7
+Operating System :: POSIX :: Linux
 '''.strip().splitlines()
 )
